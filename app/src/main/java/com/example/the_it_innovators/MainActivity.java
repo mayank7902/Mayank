e7ecf2e5ee7c74111277598a9fe078c7f8adfc62
Tanmay Suryawanshi <tanmaysuryawanshi123@gmail.com>
2022-08-02 20:24:02 +0530

    splash screen and enternumber added

--- app/src/main/java/com/example/the_it_innovators/MainActivity.java
+++ app/src/main/java/com/example/the_it_innovators/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.the_it_innovators;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Animation topAnim,botAnim;
+    ImageView image;
+    TextView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image=findViewById(R.id.imageView);
+        logo=findViewById(R.id.textViewName);
+
+        logo.setAnimation(botAnim);
+
+        image.setAnimation(topAnim);
+        Intent intent =new Intent(this,enternumber.class);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
---
+++ app/src/main/java/com/example/the_it_innovators/enternumber.java
@@ -0,0 +1,128 @@
+package com.example.the_it_innovators;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
+public class enternumber extends AppCompatActivity {
+
+
+    EditText mgetphonenumber;
+    android.widget.Button msendotp;
+    CountryCodePicker mcountrycodepicker;
+    String countrycode;
+    String phonenumber;
+    FirebaseAuth firebaseAuth;
+    ProgressBar mprogressbarofmain;
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String codesent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enternumber);
+
+
+
+        mcountrycodepicker=findViewById(R.id.countryCodePicker);
+        msendotp=findViewById(R.id.sendotpbutton);
+        mgetphonenumber=findViewById(R.id.getphonenumber);
+        mprogressbarofmain=findViewById(R.id.progressbarofmain);
+        firebaseAuth=FirebaseAuth.getInstance();
+        countrycode=mcountrycodepicker.getSelectedCountryCodeWithPlus();
+        mcountrycodepicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                countrycode=mcountrycodepicker.getSelectedCountryCodeWithPlus();
+            }
+        });
+
+
+        msendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number;
+                number=mgetphonenumber.getText().toString();
+                if(number.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please enter number", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(number.length()<10){
+                    Toast.makeText(getApplicationContext(), "Please Enter Correct Number", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Log.d("mainact", "onClick: before");
+                    mprogressbarofmain.setVisibility(View.VISIBLE);
+                    phonenumber=countrycode+number;
+
+                    PhoneAuthOptions options=PhoneAuthOptions.newBuilder(firebaseAuth)
+                            .setPhoneNumber(phonenumber)
+                            .setTimeout(60L, TimeUnit.SECONDS)
+                            .setActivity(enternumber.this)
+                            .setCallbacks(mCallbacks)
+                            .build();
+
+                    PhoneAuthProvider.verifyPhoneNumber(options);
+                }
+
+            }
+        });
+
+
+        mCallbacks=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                // how to automatically fetch code
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(getApplicationContext(), "OTP send", Toast.LENGTH_SHORT).show();
+                mprogressbarofmain.setVisibility(View.INVISIBLE);
+                codesent=s;
+                Intent intent=new Intent(enternumber.this,otpVerification.class);
+                intent.putExtra("otp",codesent);
+                startActivity(intent);
+
+            }
+        };
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+            //change chatactivity to home when home is created
+            Intent intent=new Intent(enternumber.this,chatActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+
+
+    }
+}
