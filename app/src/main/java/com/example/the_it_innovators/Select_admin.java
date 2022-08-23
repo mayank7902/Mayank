65c335f0f918b3a688d0bf6090778ccee1eaecf8
Komal <komal.22010509@viit.ac.in>
2022-08-23 12:10:24 +0530

    select scrren

---
+++ app/src/main/java/com/example/the_it_innovators/Select_admin.java
@@ -0,0 +1,16 @@
+package com.example.the_it_innovators;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.the_it_innovators.R;
+
+public class Select_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_admin);
+    }
+}
