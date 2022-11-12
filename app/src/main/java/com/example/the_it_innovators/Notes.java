34419eb6abb2ec2538c393cebbda4a05099dda65
mayank7902 <mayankchordiya7@gmail.com>
2022-11-12 19:11:39 +0530

    This is Home page of Teacher section

--- app/src/main/java/com/example/the_it_innovators/Notes.java
+++ app/src/main/java/com/example/the_it_innovators/Notes.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TeacherHome extends AppCompatActivity {
+public class Notes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_home);
+        setContentView(R.layout.activity_main);
     }
 }
