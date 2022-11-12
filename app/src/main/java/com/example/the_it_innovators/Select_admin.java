b933a940450a107e556767ef7446073e789fad1e
mayank7902 <mayankchordiya7@gmail.com>
2022-11-12 23:01:55 +0530

    This is Home page of Teacher section

--- app/src/main/java/com/example/the_it_innovators/Select_admin.java
+++ app/src/main/java/com/example/the_it_innovators/Select_admin.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.the_it_innovators.R;
-
 public class Select_admin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_admin);
+        setContentView(R.layout.activity_main);
+
     }
 }
