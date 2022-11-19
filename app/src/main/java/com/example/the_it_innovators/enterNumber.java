3cc48efc66a70dd9b30a036da6e191f69306fa9c
Tanmay Suryawanshi <tanmaysuryawanshi123@gmail.com>
2022-11-20 02:46:16 +0530

    otp verificaiton done

--- app/src/main/java/com/example/the_it_innovators/enterNumber.java
+++ app/src/main/java/com/example/the_it_innovators/enterNumber.java
@@ -1,27 +1,129 @@
 package com.example.the_it_innovators;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class enterNumber extends AppCompatActivity {
-Button sendOtpButton;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
+public class enterNumber<FirebaseAuth> extends AppCompatActivity {
+Button msendotp;
+    EditText mgetphonenumber;
+
+    CountryCodePicker mcountrycodepicker;
+    String countrycode;
+    String phonenumber;
+    com.google.firebase.auth.FirebaseAuth firebaseAuth;
+    ProgressBar mprogressbarofmain;
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String codesent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_number);
-       sendOtpButton= findViewById(R.id.sendotpbutton);
-       getSupportActionBar().hide();
-        sendOtpButton.setOnClickListener(new View.OnClickListener() {
+        msendotp= findViewById(R.id.sendotpbutton);
+       //getSupportActionBar().hide();
+        mcountrycodepicker=findViewById(R.id.countryCodePicker);
+
+        mgetphonenumber=findViewById(R.id.getphonenumber);
+        mprogressbarofmain=findViewById(R.id.progressbarofmain);
+        firebaseAuth= com.google.firebase.auth.FirebaseAuth.getInstance();
+        countrycode=mcountrycodepicker.getSelectedCountryCodeWithPlus();
+        mcountrycodepicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent =new Intent(enterNumber.this,otpVerification.class);
-                startActivity(intent);
-                finish();
+            public void onCountrySelected() {
+                countrycode=mcountrycodepicker.getSelectedCountryCodeWithPlus();
             }
         });
+
+
+        msendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number;
+                number=mgetphonenumber.getText().toString();
+                if(number.isEmpty()){
+                    Toast.makeText(enterNumber.this, "Please enter number", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(number.length()<10){
+                    Toast.makeText(enterNumber.this, "Please Enter Correct Number", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Log.d("mainact", "onClick: before");
+                    mprogressbarofmain.setVisibility(View.VISIBLE);
+                    phonenumber=countrycode+number;
+
+                    PhoneAuthOptions options=PhoneAuthOptions.newBuilder(firebaseAuth)
+                            .setPhoneNumber(phonenumber)
+                            .setTimeout(60L, TimeUnit.SECONDS)
+                            .setActivity(enterNumber.this)
+                            .setCallbacks(mCallbacks)
+                            .build();
+
+                    PhoneAuthProvider.verifyPhoneNumber(options);
+                }
+
+            }
+        });
+
+
+        mCallbacks=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                // how to automatically fetch code
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(getApplicationContext(), "OTP send", Toast.LENGTH_SHORT).show();
+                mprogressbarofmain.setVisibility(View.INVISIBLE);
+                codesent=s;
+                Intent intent=new Intent(enterNumber.this,otpVerification.class);
+                intent.putExtra("otp",codesent);
+                startActivity(intent);
+
+
+            }
+        };
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(com.google.firebase.auth.FirebaseAuth.getInstance().getCurrentUser()!=null){
+
+            Intent intent=new Intent(enterNumber.this,Home_screen.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/the_it_innovators/otpVerification.java
+++ app/src/main/java/com/example/the_it_innovators/otpVerification.java
@@ -1,27 +1,90 @@
 package com.example.the_it_innovators;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class otpVerification extends AppCompatActivity {
+    TextView mchangenumber;
+    EditText mgetotp;
+    Button mverifyotp;
+    String enterotp;
+    FirebaseAuth firebaseAuth;
+    ProgressBar mprogressbarofotpauth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verification);
         getSupportActionBar().hide();
-        Button verify=findViewById(R.id.buttonVerifyOTP);
-        verify.setOnClickListener(new View.OnClickListener() {
+
+        mverifyotp=findViewById(R.id.buttonVerifyOTP);
+        mgetotp=findViewById(R.id.editTextOTP);
+        mprogressbarofotpauth=findViewById(R.id.progressbarofOTPVerification);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+
+        mverifyotp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(otpVerification.this,Home_screen.class);
-                startActivity(intent);
-                finish();
+                enterotp=mgetotp.getText().toString();
+                if(enterotp.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Enter opt first", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    mprogressbarofotpauth.setVisibility(View.VISIBLE);
+                    String coderecieved=getIntent().getStringExtra("otp");
+                    PhoneAuthCredential credential= PhoneAuthProvider.getCredential(coderecieved,enterotp);
+                    signInWithPhoneAuthCredential(credential);
+
+                }
+
+
+            }
+        });
+
+
+    }
+
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential){
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    mprogressbarofotpauth.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(otpVerification.this,Home_screen.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    if(task.getException() instanceof FirebaseAuthInvalidCredentialsException){
+                        mprogressbarofotpauth.setVisibility(View.INVISIBLE);
+                        Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
-}
+
+
+    }
